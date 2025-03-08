aa1dc66e8aed665d52ba4fce3caf737562ab28c2
Luiz Gustavo <luizgomaciel@gmail.com>
2025-03-08 17:33:27 -0300

    completando o desafio de Domain Event da FC - DDD: Modelagem Tática e Patterns

---
+++ src/domain/customer/event/handler/envia-console-log-2.handler.ts
@@ -0,0 +1,11 @@
+import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
+import CustomerCreatedEvent from "../customer-created.event";
+
+export default class EnviaConsoleLog1Handler
+  implements EventHandlerInterface<CustomerCreatedEvent>
+{
+  handle(event: CustomerCreatedEvent): void {
+    console.log(`Esse é o segundo console.log do evento: CustomerCreated`);
+    console.log(`Endereço do cliente: ${event.eventData.id}, ${event.eventData.nome} alterado para: ${event.eventData.endereco}`);
+  }
+}
--- src/domain/customer/factory/customer.factory.ts
+++ src/domain/customer/factory/customer.factory.ts
@@ -1,10 +1,26 @@
 import Customer from "../entity/customer";
 import { v4 as uuid } from "uuid";
 import Address from "../value-object/address";
+import EnviaConsoleLog1Handler from "../event/handler/envia-console-log-1.handler"
+import EventDispatcher from "../../@shared/event/event-dispatcher";
+import CustomerCreatedEvent from "../event/customer-created.event";
 
 export default class CustomerFactory {
   public static create(name: string): Customer {
-    return new Customer(uuid(), name);
+    const customer = new Customer(uuid(), name);
+    const eventDispatcher = new EventDispatcher();
+
+    const customerCreatedEvent = new CustomerCreatedEvent({
+      id: customer.id,
+      nome: customer.name,
+    });
+
+    const eventHandler = new EnviaConsoleLog1Handler();
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler);
+    eventDispatcher.notify(customerCreatedEvent);
+
+    return customer;
   }
 
   public static createWithAddress(name: string, address: Address): Customer {
